329cb9a80397edd8bf246a63192aae1ec665762c
ThienNgoDong <thienngodong9b123@gmail.com>
2021-04-21 00:54:07 +0700

    test
    TEST

--- NoteManagementSystem/app/src/main/java/com/example/notemanagementsystem/MainActivity.java
+++ NoteManagementSystem/app/src/main/java/com/example/notemanagementsystem/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test
         //SignUp Account
         btnSignUp = findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
